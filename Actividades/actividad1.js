006b6e7af8b38753a50e1e1f5938b536aa6234e1
rusanrod <rsandovr@gmail.com>
2022-05-31 19:43:13 -0500

    Actividad 1

---
+++ Actividades/actividad1.js
@@ -0,0 +1,22 @@
+//Actividad 1, problema 1
+var num = prompt("ingresa un número")
+num = num * 2
+alert(num)
+
+//Actividad 1, problema 2
+var num1 = prompt("ingresa un número")
+var num2 = prompt("ingresa otro número")
+var resta = num1 - num2
+alert(resta)
+
+//Actividad 1, problema 3
+const pi= 3.1416
+const radio = 5.3
+const areaCirculo = pi*(radio**2)
+alert("el área es " + areaCirculo) 
+
+//Actividad 1, problema 4
+const D = 8 //cm
+const d = 6 //cm
+const areaRombo = (D * d)/2
+alert("el área es " + areaRombo)
