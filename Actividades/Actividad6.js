3b6015048df414f7806ad0a637ab2c5bb49c4122
rusanrod <rsandovr@gmail.com>
2022-06-02 22:14:04 -0500

    Actividad 6

---
+++ Actividades/Actividad6.js
@@ -0,0 +1,21 @@
+//Actividad 6, Ejercicio 1
+var animals= ["eagle","parrot","monkey","boar","lion"]
+var comingSoonAnimals=["panter","dragon","turtle"]
+var zoo = animals.concat(comingSoonAnimals)
+console.log(zoo)
+
+//Actividad 6, Ejercicio 2
+var array=[4,6,1,0,8,2]
+var sorted=array.sort()
+console.log(sorted)
+
+//Actividad 6, Ejercicio 3
+var animals= ["eagle","parrot","monkey","boar","lion"]
+animals.push("cow")
+console.log(animals)
+
+//Actividad 6, Ejercicio 4
+var animals= ["eagle","parrot","monkey","boar","lion"]
+var reversed=animals.reverse()
+reversed.pop()
+animals=reversed.reverse()
