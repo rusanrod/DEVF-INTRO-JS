48d23c1dd1a8b66270c8bfe01a36f7fc2a12856c
rusanrod <rsandovr@gmail.com>
2022-06-20 21:20:04 -0500

    Avances del proyecto

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -3,123 +3,215 @@ let input = ""        //lo que en realidad esta pasando
 let cState = 0      //current state
 let nState = 1      //next state
 let borrar = false  
-let message = document.getElementById("mensaje") 
-let data = document.getElementById("vis_data")
-let clientID = 0    //client ID from input 
+const message = document.getElementById("mensaje") 
+const data1 = document.getElementById("vis_data1")
+const data2 = document.getElementById("vis_data2")
+const data3 = document.getElementById("vis_data3")
+// const btn_left = document.querySelector("#btn_left")
+let clientID = 0
 let clientNIP = 0   //client NIP from input
 let cliente         //client account
-let dataEditable = false
+let dataEditable = false //allow to click any key from the main keyboard
+let lat_data = false //allow to click any key from the lateral keyboard
+let btn_input = 0
+let operation = 0 //by default 0:"check balance", 1: "deposit money", 2:"cash withdrawal"
+let clientsStored
 // console.log(data)
 
-const clients =[
-    {
-        name: "Ruben Sandoval",
-        ID: 561177,
-        NIP: 2811,
-        saldo: 15405
-    },
-    {
-        name: "Ximena Cervantes",
-        ID: 547896,
-        NIP: 2020,
-        saldo: 2514
-    },
-    {
-        name:"Abigail Rodríguez",
-        ID: 554027,
-        NIP: 1502,
-        saldo: 7845
+// document.addEventListener('DOMContentLoaded',() => {
+//     console.log(cState)
+// })
+document.addEventListener("DOMContentLoaded", () => {
+    clientsStored = JSON.parse(localStorage.getItem('clients'))
+    if(!clientsStored){
+            //add
+        console.log("no hay, pero ahora si")
+        localStorage.setItem('clients', JSON.stringify(clients))
     }
-]
+
+
+    
+})
 function key_pressed(clicked){
     if(dataEditable){
         cState == 2 ? inputString += "*" : inputString += clicked
         input += clicked
-        data.textContent = inputString
+        data2.textContent = inputString
     }
 }
 function del(){
     if(dataEditable){
         inputString = ""
         input = ""
-        data.textContent = inputString
+        data2.textContent = inputString
     } 
 }
 function check(){
     inputString = ""
-    data.textContent = inputString
+    data2.textContent = inputString
     //cState++
     state_machine(nState)
 }
+
+// btn_left.addEventListener("click", () => {
+//     console.log("click")
+// })
+function left_pressed(clicked){
+    if(lat_data){
+        let arr = ["l1", "l2", "l3"]
+        btn_input = arr.indexOf(clicked)
+        // console.log(btn_input)
+        state_machine(nState)
+    }
+}
+
 function state_machine(state){
     switch(state){
-        case 0:
+        case 0: //Start
             dataEditable = false
+            lat_data = false
             cState = 0
-            mensaje.textContent = "Hello! Press check key to start."
-            data.textContent = "_"
+            message.textContent = "Hello! Press check key to start."
+            data1.textContent = ""
+            data2.textContent = "_"
+            data3.textContent = ""
             nState = 1
         break;
-        case 1:
-            dataEditable = true
+        case 1: //Pick a name
+            dataEditable = false
+            lat_data = true
             cState = 1
-            input = ""
-            mensaje.textContent = "Type your client ID"
-            data.textContent = "_"
+            // input = ""
+            message.textContent = "Choose your name:"
+            // data.textContent = "Ruben Sandoval"
+            data1.textContent = `-${clientsStored[0].name}`
+            data2.textContent = `-${clientsStored[1].name} `
+            data3.textContent = `-${clientsStored[2].name} `
             nState = 2
         break;
-        case 2:
+        case 2: //Type nip
             dataEditable = true
+            lat_data = false
             cState = 2
-            cliente ? clientID:clientID = input 
+            // cliente ? clientID:clientID = input 
             input = ""
-            mensaje.textContent = "Type your NIP"
-            data.textContent = "_"
+            message.textContent = "Type your NIP"
+            data1.textContent = ""
+            data2.textContent = "_"
+            data3.textContent = ""
             nState = 3
         break;
-        case 3:
+        case 3: // nip validation
             dataEditable = false
+            lat_data = false
             cState = 3
             clientNIP = input
             input = ""
-            mensaje.textContent = "Validating"
-            data.textContent = "_"
-            cliente=(clients.filter(item=>item.ID==clientID))[0]
+            message.textContent = "Validating" //cambio por tiempo
+            data1.textContent = ""
+            data2.textContent = "_"
+            data3.textContent = ""
+            // cliente=(clients.filter(item=>item.ID==clientID))[0]
+            cliente = clientsStored[btn_input]
             if(cliente){    //ID correcto
-                if(cliente.NIP==clientNIP){ 
+                if(cliente.NIP == clientNIP){ 
                     nState = 4 //NIP correcto
                 }
                 else{
-                    nState = 5 //NIP incorrecto
+                    nState = 20 //NIP incorrecto
                 }
             }
-            else{   //ID incorrecto
-                console.log("entra")
-                nState = 6 
+        break;
+        case 4: //account operations
+            dataEditable = false
+            lat_data = true
+            cState = 4
+            message.textContent = "Hello " + cliente.name + "!"
+            data1.textContent = "-Check balance"
+            data2.textContent = "-Deposit money"
+            data3.textContent = "-Cash withdrawal"
+            // cliente=undefined
+            nState = 5
+        break;
+        case 5: //validation 
+            dataEditable = false
+            lat_data = false
+            cState = 5
+            operation = btn_input
+            if(operation == 0){
+                // check balance
+                nState = 6
+            }
+            else if (operation == 1){
+                // deposit money
+                nState = 7
+            }
+            else{
+                // cash withdrawal
+                nState = 8
+            }
+
+            break;
+
+        case 6: //check balance
+            dataEditable = false
+            lat_data = false
+            cState = 6
+            message.textContent = "Your balance is:"
+            data1.textContent = ""
+            data2.textContent = "$" + cliente.saldo
+            data3.textContent = ""
+            nState = 4
+        break;
+        case 7: //deposit money
+            dataEditable = true
+            lat_data = false
+            cState = 7
+            input = ""
+            message.textContent = "Type your deposit:"
+            data1.textContent=""
+            data2.textContent = "_"
+            data3.textContent = ""
+            nState = 9
+        break;
+        case 8: //cash withdrawal
+            dataEditable = false
+            lat_data = false
+            input = ""
+            message.textContent = "Type your withdrawal:"
+            data1.textContent=""
+            data2.textContent = "_"
+            data3.textContent = ""
+            cState = 9
+
+        break;
+
+        case 9: //operation validation
+            dataEditable = false
+            lat_data = false
+            if (operation==2){
+                let nuevoSaldo = cliente.saldo + Number(input)
+                if(nuevoSaldo > 990){
+                    nState = 28 //error
+                }
+                else{
+                    cliente.saldo = nuevoSaldo
+                    clientsStored.push(cliente)
+                    console.log(clientsStored) 
+                    nState = 10 // aun no se
+                }
+                
             }
         break;
-        case 4:
-            dataEditable = false
-            cState = 4
-            dataEditable=false
-            mensaje.textContent = "Hello " + cliente.name + " your balance is:" 
-            data.textContent = "$" + cliente.saldo
-            cliente=undefined
-            nState = 0
+
+        case 20: // wrong nip
+        dataEditable = false
+        lat_data = false
+        cState = 20
+        message.textContent = "Wrong NIP"
+        data2.textContent = "XXXX"
+        nState = 2 //por tiempo deberia pasar al siguiente estado
         break;
-        case 5:
-            dataEditable = false
-            cState = 5
-            mensaje.textContent = "Wrong NIP"
-            data.textContent = "XXXX"
-            nState = 2
-        break;
-        case 6:
-            dataEditable = false
-            cState = 6
-            mensaje.textContent = "Client not found"
-            data.textContent = "XXXXXX"
-            nState = 1
-        break;
+
     }
 }
---
+++ Proyecto/cuentas.js
@@ -0,0 +1,20 @@
+const clients =[ //saldo entre 10 y 990
+    {
+        name: "Ruben Sandoval",
+        ID: 561177,
+        NIP: 2811,
+        saldo: 595
+    },
+    {
+        name: "Ximena Cervantes",
+        ID: 547896,
+        NIP: 2020,
+        saldo: 710
+    },
+    {
+        name:"Abigail Rodríguez",
+        ID: 554027,
+        NIP: 1502,
+        saldo: 243
+    }
+]
